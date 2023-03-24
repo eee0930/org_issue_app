752109fca8ef1bde3615c40f27fe14f6b76fd690
lazyhysong <eee0930@naver.com>
2023-03-24 14:28:03 +0900

    style: list page 간격 조절

--- src/utils/listIssuesStyles.ts
+++ src/utils/listIssuesStyles.ts
@@ -1,5 +1,64 @@
 import styled from "styled-components";
 
+export const IssueListContainer = styled.div`
+    margin: 20px auto;
+    width: 100%;
+    max-width: 1300px;
+`;
+export const Modal = styled.div`
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    z-index: 50;
+    transform: translate(-50%, -50%);
+    border-radius: 8px;
+    width: 300px;
+    height: 100px;
+    background-color: #fff;
+    color: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+export const Footer = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    z-index: 100;
+    background-color: ${props => props.theme.black.veryDark};
+    .col-6 {
+        padding: 15px 10px;
+    }
+    @media only screen and (min-width: 768px) {
+        .col-6 {
+            padding: 15px;
+            text-align: center;
+        }
+    }
+`;
+const Button = styled.button`
+    width: 100%;
+    height: 40px;
+    border-radius: 8px;
+    border: 1px solid #000;
+    font-size: 16px;
+    @media only screen and (min-width: 768px) {
+        width: 200px;
+    }
+`;
+export const GetButton = styled(Button)`
+    border-color: ${props => props.theme.green.darker};
+    background-color: ${props => props.theme.green.darker};
+    color: ${props => props.theme.white.lighter};
+`;
+export const ResetButton = styled(Button)`
+    border-color: ${props => props.theme.white.lighter};
+    background-color: ${props => props.theme.white.lighter};
+    color: ${props => props.theme.green.darker};
+`;
+
 export const AdImg = styled.img`
     height: 65px;
     width: auto;
