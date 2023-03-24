551293ecb4baf60c721f1d5e1c4784907f46b8e8
lazyhysong <eee0930@naver.com>
2023-03-24 19:18:10 +0900

    style: 줄 변경

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -3,32 +3,32 @@ import styled from "styled-components";
 import { isDarkThemeState } from "../atoms";
 
 const Button = styled.button`
-    width: 100px;
-    height: 40px;
-    border-radius: 23px;
-    border: solid 2px ${props => props.theme.green.lighter};
-    font-size: 13px;
-    background-color: transparent;
-    color: ${props => props.theme.white.lighter};
-    position: absolute;
-    top: calc(50% - 20px);
-    right: 20px;
+  width: 100px;
+  height: 40px;
+  border-radius: 23px;
+  border: solid 2px ${props => props.theme.green.lighter};
+  font-size: 13px;
+  background-color: transparent;
+  color: ${props => props.theme.white.lighter};
+  position: absolute;
+  top: calc(50% - 20px);
+  right: 20px;
 `;
 
 interface IThemeButton {
-    text: string;
+  text: string;
 }
 function ThemeButton({text}: IThemeButton) {
-    const setTheme = useSetRecoilState(isDarkThemeState);
-    const handleThemeButton = () => {
-        setTheme((prev) => !prev);
-    };
-    
-    return (
-        <Button onClick={handleThemeButton}>
-            {text}
-        </Button>
-    );   
+  const setTheme = useSetRecoilState(isDarkThemeState);
+  const handleThemeButton = () => {
+    setTheme((prev) => !prev);
+  };
+  
+  return (
+    <Button onClick={handleThemeButton}>
+      {text}
+    </Button>
+  );   
 }
 
 export default ThemeButton;
